6c2da8096e2ded7bd347cc72b7913b72bcd1334b
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-03-30 00:43:55 +0500

    reducer added ( increase, decrease & total amount)

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,12 +1,14 @@
 import { ChevronDown, ChevronUp } from "../icons";
 import { useDispatch } from "react-redux";
-import { removeItem } from "../features/cart/cartSlice";
+import { removeItem, increase, decrease } from "../features/cart/cartSlice";
 
 const CartItem = ({ id, img, title, price, amount }) => {
   const dispatch = useDispatch();
+
   return (
     <article className="cart-item">
       <img src={img} alt={title} />
+
       <div>
         <h4>{title}</h4>
         <h4 className="item-price">${price}</h4>
@@ -16,11 +18,25 @@ const CartItem = ({ id, img, title, price, amount }) => {
       </div>
 
       <div>
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => dispatch(increase({ id }))}
+        >
           <ChevronUp />
         </button>
+
         <p className="amount">{amount}</p>
-        <button className="amount-btn">
+
+        <button
+          className="amount-btn"
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+            }
+            dispatch(decrease({ id }));
+          }}
+        >
           <ChevronDown />
         </button>
       </div>
